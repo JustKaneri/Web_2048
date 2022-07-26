6d4f53be714d0688b8ad228a04085e32f9636fca
JustKaneri <justkaneri@yandex.ru>
2022-07-26 19:25:16 +0300

    адаптивность, начало логики

---
+++ scripts/game_logic.js
@@ -0,0 +1,10 @@
+var Scope = 0;
+var GameMap = [[-1,-1,-1,-1],
+			   [-1,-1,-1,-1],
+			   [-1,-1,-1,-1],
+			   [-1,-1,-1,-1]];
+
+
+var LbxScope = document.getElementsByClassName('main_ui__label_scope');
+
+LbxScope[0].innerHTML = "404";
